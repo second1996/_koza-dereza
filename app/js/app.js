050edbe451ad06f30905daf3b2329b0abc616afe
second1996 <vital10012@gmail.com>
2020-08-26 15:58:55 +0300

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	var homeSlider = new Swiper('.heroes-slider', {
+	var homeSlider = new Swiper('.h-heroes-slider', {
 		loop: false,
 		effect: 'fade',
 		fadeEffect: {
 			crossFade: true
 		},
 		pagination: {
-			el: '.swiper-pagination',
+			el: '.h-heroes .swiper-pagination',
+			clickable: true,
+		},
+	})
+	var reviewsSlider = new Swiper('.h-reviews-slider', {
+		slidesPerView: 2,
+		spaceBetween: 20,
+		speed: 700,
+		autoplay: {
+			delay: 2000
+		},
+		// loop: false,
+		pagination: {
+			el: '.h-reviews .swiper-pagination',
 			clickable: true,
 		},
 	})
